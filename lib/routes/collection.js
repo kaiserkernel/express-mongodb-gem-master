andzdroid/mongo-express

feat: support aggregate query in advanced tab
with checkbox opt-in

lib/routes/collection.js
@@ -31,12 +31,13 @@ const routes = function (config) {
    * Simple/Advanced parameters input.
    * Returns null if no query parameters were passed in request.
    */
-  exp._buildMongoQuery = function (req) {
+  exp._buildMongoQuery = function (req, res, queryOptions) {
     let result = null;
     const { key } = req.query;
     let { value } = req.query;
     const type = req.query.type && req.query.type.toUpperCase();
     const jsonQuery = req.query.query;
+    const runAggregate = !!req.query.runAggregate;
 
     if (key && value) {
       // if it is a simple query,
@@ -78,6 +79,21 @@ const routes = function (config) {
       if (result === null) {
         throw new Error('Query entered is not valid');
       }
+      if (runAggregate) {
+        // if it's an aggregate query, add $project for projection and $facet for pagination
+        result = [
+          ...result,
+          ...(queryOptions.projection && Object.keys(queryOptions.projection).length ? [{
+            $project: queryOptions.projection,
+          }] : []),
+          {
+            $facet: {
+              metadata: [{ $count: 'total' }],
+              data: [{ $skip: queryOptions.skip }, { $limit: queryOptions.limit }],
+            },
+          },
+        ];
+      }
     }
     // otherwise leave as null;
     return result;
@@ -111,8 +127,8 @@ const routes = function (config) {
     return projection;
   };
 
-  exp._getQuery = function (req, res) {
-    return exp._buildMongoQuery(req, res) || {};
+  exp._getQuery = function (req, res, queryOptions) {
+    return exp._buildMongoQuery(req, res, queryOptions) || {};
   };
 
   // view all entries in a collection
@@ -122,8 +138,8 @@ const routes = function (config) {
     let query;
     let queryOptions;
     try {
-      query = exp._getQuery(req, res);
       queryOptions = exp._getQueryOptions(req);
+      query = exp._getQuery(req, res, queryOptions);
     } catch (err) {
       req.session.error = err.message;
       return res.redirect('back');
@@ -150,14 +166,30 @@ const routes = function (config) {
     const { limit } = queryOptions;
     const { sort } = queryOptions;
 
-    req.collection.find(query, queryOptions).toArray(function (err, items) {
+    const runAggregate = !!req.query.runAggregate;
+
+    const results = runAggregate ? req.collection.aggregate(query) : req.collection.find(query, queryOptions);
+
+    results.toArray(function (err, resultArray) {
+      const items = runAggregate ? resultArray && resultArray[0] && resultArray[0].data || [] : resultArray;
+      const getCount = runAggregate
+        ? (_, __, cb) => cb(
+          null,
+          resultArray
+          && resultArray[0]
+          && resultArray[0].metadata
+          && resultArray[0].metadata[0]
+          && resultArray[0].metadata[0].total
+          || 0,
+        )
+        : (...args) => req.collection.count(...args);
       req.collection.stats(function (err, stats) {
         if (stats === undefined) {
           req.session.error = 'Collection not found!';
           return res.redirect('back');
         }
         req.collection.indexes(function (err, indexes) {
-          req.collection.count(query, null, function (err, count) {
+          getCount(query, null, function (err, count) {
             // Pagination
             // Have to do this here, swig template doesn't allow any calculations :(
             const prev = {
@@ -253,6 +285,7 @@ const routes = function (config) {
               type: req.query.type,
               query: req.query.query,
               projection: req.query.projection,
+              runAggregate,
               defaultKey,
               edKey,
               indexes,

kaiserkernel/express-mongodb-gem-master

lib/routes/collection.js
@@ -30,12 +30,13 @@ const routes = function (config) {
    * Simple/Advanced parameters input.
    * Returns null if no query parameters were passed in request.
    */
-  exp._buildMongoQuery = function (req) {
+  exp._buildMongoQuery = function (req, res, queryOptions) {
     let result = null;
     const { key } = req.query;
     let { value } = req.query;
     const type = req.query.type && req.query.type.toUpperCase();
     const jsonQuery = req.query.query;
+    const runAggregate = !!req.query.runAggregate;
 
     if (key && value) {
       // if it is a simple query,
@@ -77,6 +78,21 @@ const routes = function (config) {
       if (result === null) {
         throw new Error('Query entered is not valid');
       }
+      if (runAggregate) {
+        // if it's an aggregate query, add $project for projection and $facet for pagination
+        result = [
+          ...result,
+          ...(queryOptions.projection && Object.keys(queryOptions.projection).length ? [{
+            $project: queryOptions.projection,
+          }] : []),
+          {
+            $facet: {
+              metadata: [{ $count: 'total' }],
+              data: [{ $skip: queryOptions.skip }, { $limit: queryOptions.limit }],
+            },
+          },
+        ];
+      }
     }
     // otherwise leave as null;
     return result;
@@ -110,8 +126,8 @@ const routes = function (config) {
     return projection;
   };
 
-  exp._getQuery = function (req, res) {
-    return exp._buildMongoQuery(req, res) || {};
+  exp._getQuery = function (req, res, queryOptions) {
+    return exp._buildMongoQuery(req, res, queryOptions) || {};
   };
 
   // view all entries in a collection
@@ -121,8 +137,8 @@ const routes = function (config) {
     let query;
     let queryOptions;
     try {
-      query = exp._getQuery(req, res);
       queryOptions = exp._getQueryOptions(req);
+      query = exp._getQuery(req, res, queryOptions);
     } catch (err) {
       req.session.error = err.message;
       return res.redirect('back');
@@ -146,16 +162,28 @@ const routes = function (config) {
     };
 
     const { limit, skip, sort } = queryOptions;
+    const runAggregate = !!req.query.runAggregate;
 
     try {
-      const items = await req.collection.find(query, queryOptions).toArray();
+      const resultArray = await (runAggregate
+        ? req.collection.aggregate(query).toArray()
+        : req.collection.find(query, queryOptions).toArray()
+      );
+      const items = runAggregate ? resultArray && resultArray[0] && resultArray[0].data || [] : resultArray;
       const stats = await req.collection.stats();
       if (stats === undefined) {
         req.session.error = 'Collection not found!';
         return res.redirect('back');
       }
       const indexes = await req.collection.indexes();
-      const count = await req.collection.countDocuments(query);
+      const count = runAggregate
+        ? resultArray
+          && resultArray[0]
+          && resultArray[0].metadata
+          && resultArray[0].metadata[0]
+          && resultArray[0].metadata[0].total
+          || 0
+        : await req.collection.countDocuments(query);
       // Pagination
       // Have to do this here, swig template doesn't allow any calculations :(
       const prev = {
@@ -251,6 +279,7 @@ const routes = function (config) {
         type: req.query.type,
         query: req.query.query,
         projection: req.query.projection,
+        runAggregate,
         defaultKey,
         edKey,
         indexes,